7e81275be889e6ade69121196f6dd7a20c043754
Takuji Okubo <takuji.okubo@gmail.com>
2020-07-18 15:41:02 +1000

    update

--- client/src/components/Nabbar.js
+++ client/src/components/Nabbar.js
@@ -4,7 +4,7 @@ function Nabbar(){
     return (
         <div className="jumbotron jumbotron-fluid bg-dark text-white">
             <div className="container">
-            <h1 className="display-4">Employee Directory</h1>
+            <h1 className="display-4">Employee Directory on Beanstalk</h1>
             <p className="lead">Click on an arrow to sort by heading or use the search box to narrow your results.</p>
             </div>
         </div>
